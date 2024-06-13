1b34d211e2827234853124b01b4d342026364f17
sebas1231232 <sebastian.pino37@inacapmail.cl>
2024-06-13 19:45:01 -0400

    mensaje

---
+++ firestore.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+import { addDoc, collection, deleteDoc, doc, getDoc, getFirestore, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCciAYK7sqGljRnjKljvgeXCMJzv85G1U",
+  authDomain: "actividad1-86ad4.firebaseapp.com",
+  projectId: "actividad1-86ad4",
+  storageBucket: "actividad1-86ad4.appspot.com",
+  messagingSenderId: "439746215965",
+  appId: "1:439746215965:web:13ec021871e515b5a486f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const db = getFirestore(app)
+
+export const save = (clas) => {
+    addDoc(collection(db, 'Clasificacion'), clas)
+}
+
+export const getData = (data) => {
+    onSnapshot(collection(db, 'Clasificacion'), data)
+}
+
+export const remove = (id) => {
+    deleteDoc(doc(db, 'Clasificacion', id))
+}
+
+export const getDocumento = (id) => getDoc(doc(db, 'Clasificacion', id))
+
+export const update = (id, clasificacion) =>{
+    updateDoc(doc(db,'Clasificacion',id),clasificacion)
+}
---
+++ funciones.js
@@ -0,0 +1,101 @@
+import { getData, save, remove, getDocumento, update } from "./firestore.js"
+let id = 0
+
+document.getElementById('btnGuardar').addEventListener('click', () => {
+    document.querySelectorAll('.form-control').forEach(item => {
+        verificar(item.id)
+    })
+    if (document.querySelectorAll('.is-invalid').length == 0) {
+        const jugador = {
+            nick: document.getElementById('nick').value,
+            fecha: document.getElementById('fecha').value,
+            punt: document.getElementById('punt').value,
+            tiempo: document.getElementById('tiempo').value,
+            juego: document.getElementById('juego').value,
+            dif: document.getElementById('dif').value,
+            clasi: document.getElementById('clasi').value,
+        }
+        if (document.getElementById('btnGuardar').value == 'Guardar') {
+            save(jugador)
+        } 
+        else {
+            update(id, jugador)
+            id = 0
+        }    
+        limpiar()
+    }
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    getData((datos) => {
+        let tabla = ''
+
+        datos.forEach((doc) => {
+
+            const item = doc.data()
+            tabla += `<tr>
+            <td>${item.nick}</td>
+            <td>${item.fecha}</td>
+            <td>${item.punt}</td>
+            <td>${item.tiempo}</td>
+            <td>${item.juego}</td>
+            <td>${item.dif}</td>
+            <td>${item.clasi}</td>
+            <td nowrap>
+                <button class="btn btn-warning" id="${doc.id}">Editar</button>
+                <button class="btn btn-danger" id="${doc.id}">Eliminar</button>
+            </td>
+        </tr>`
+        })
+        document.getElementById('contenido').innerHTML = tabla
+
+        document.querySelectorAll('.btn-danger').forEach(btn => {
+
+            btn.addEventListener('click', () => {
+                Swal.fire({
+                    title: "¿Está seguro que desea eliminar el registro?",
+                    text: "No podrás revertir los cambios",
+                    icon: "error",
+                    showCancelButton: true,
+                    confirmButtonColor: "#d33",
+                    cancelButtonColor: "#3085d6",
+                    confirmButtonText: "Eliminar"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+
+                        remove(btn.id)
+                        Swal.fire({
+                            title: "Eliminado!",
+                            text: "Su registro ha sido eliminado",
+                            icon: "success"
+                        })
+                    }
+                })
+            })
+        })
+
+        document.querySelectorAll('.btn-warning').forEach(btn => {
+
+            btn.addEventListener('click', async () => {
+
+                const clasificacion = await getDocumento(btn.id)
+
+                const c = clasificacion.data()
+
+                document.getElementById('nick').value = c.nick
+                document.getElementById('fecha').value = c.fecha
+                document.getElementById('puntuacion').value = c.punt
+                document.getElementById('tiempo').value = c.tiempo
+                document.getElementById('juego').value = c.juego
+                document.getElementById('dif').value = c.dif
+                document.getElementById('clasi').value = c.clasi
+
+                document.getElementById('btnGuardar').value = 'Editar'
+
+                document.getElementById('nick').readOnly = true
+
+                id = clasificacion.id
+            })
+        })
+    })
+})
